3723980a7bc997fccf8cc75a39bd2bea471638d4
ryichang <ryichang@gmail.com>
2016-02-27 19:44:03 -0800

    Added Controller seeded data, script.js for filter function, include ng-app and ng-controller in body

---
+++ controller.js
@@ -0,0 +1,39 @@
+// The controller
+
+function SearchController($scope){
+
+	$scope.items = [
+		{'id': '0',
+	    'title': 'Tatcha Camellia Cleansing Oil Face Wash',
+	    'image': 'http://ecx.images-amazon.com/images/I/31vmEb7lWrL._SL160_.jpg',
+	    'price': '$48.00',
+	    'url': 'http://www.amazon.com/TATCHA-Camellia-Cleansing-Face-Wash/dp/B00E5DPP62%3FSubscriptionId%3DAKIAI6IJ2HVTJ4MH6V3A%26tag%3Dtatchachallen-20%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3DB00E5DPP62'
+	},
+    {'id': '1',
+	    'title': 'Tatcha Classic Rice Enzyme Powder Exfoliant',
+	    'image': 'http://ecx.images-amazon.com/images/I/419cpgu7pcL._SL160_.jpg',
+	    'price': '$65.00',
+	    'url': 'http://www.amazon.com/TATCHA-Classic-Enzyme-Powder-Exfoliant/dp/B00IA6MRI4%3FSubscriptionId%3DAKIAI6IJ2HVTJ4MH6V3A%26tag%3Dtatchachallen-20%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3DB00IA6MRI4'	
+	      },
+    {'id': '2',
+        'title': 'Tatcha Original Aburatorigami',
+        'image': 'http://ecx.images-amazon.com/images/I/41Dgvwn7W5L._SL160_.jpg',
+        'price': '$12.00',
+        'url': 'http://www.amazon.com/Tatcha-Original-Aburatorigami/dp/B003D79TEA%3FSubscriptionId%3DAKIAI6IJ2HVTJ4MH6V3A%26tag%3Dtatchachallen-20%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3DB003D79TEA'
+          },
+    {'id': '3',
+        'title': 'Tatcha Luminous Dewy Skin Mist Travel Size 0.4 oz',
+        'image': 'http://ecx.images-amazon.com/images/I/41qWPccoquL._SL160_.jpg',
+        'price': '$29.00',
+        'url': 'http://www.amazon.com/Tatcha-LUMINOUS-DEWY-SKIN-TRAVEL/dp/B00W66NRME%3FSubscriptionId%3DAKIAI6IJ2HVTJ4MH6V3A%26tag%3Dtatchachallen-20%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3DB00W66NRME'       
+          },
+    {'id': '4',
+        'title': 'Tatcha Luminous Dewy Skin Mist',
+        'image': 'http://ecx.images-amazon.com/images/I/21NBESVRiAL._SL160_.jpg',
+        'price': '$61.05',
+        'url': 'http://www.amazon.com/Tatcha-Luminous-Dewy-Skin-Mist/dp/B015LGI3T2%3FSubscriptionId%3DAKIAI6IJ2HVTJ4MH6V3A%26tag%3Dtatchachallen-20%26linkCode%3Dxm2%26camp%3D2025%26creative%3D165953%26creativeASIN%3DB015LGI3T2' 
+          },            
+  ];
+
+
+}
---
+++ script.js
@@ -0,0 +1,31 @@
+// Define a new module for our app
+var app = angular.module("Tatcha", []);
+
+// Create the instant search filter
+
+app.filter('searchFor', function(){
+
+
+	return function(arr, searchString){
+
+		if(!searchString){
+			return arr;
+		}
+
+		var result = [];
+
+		searchString = searchString.toLowerCase();
+
+		// Using the forEach helper method to loop through the array
+		angular.forEach(arr, function(item){
+
+			if(item.title.toLowerCase().indexOf(searchString) !== -1){
+				result.push(item);
+			}
+
+		});
+
+		return result;
+	};
+
+});
